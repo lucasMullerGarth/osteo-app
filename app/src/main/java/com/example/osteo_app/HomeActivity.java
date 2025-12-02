a712f42fa200fd12e46372e4c8f26a3a8da80b18
Darkcomet51 <leonardoaamuller@gmail.com>
2025-12-02 17:27:33 -0300

    mais um commit

--- app/src/main/java/com/example/osteo_app/HomeActivity.java
+++ app/src/main/java/com/example/osteo_app/HomeActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button tecnicasdealivioButton;
+    private Button educacaoButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        tecnicasdealivioButton = findViewById(R.id.btnRelief);
+        educacaoButton = findViewById(R.id.btnEducation);
+
+        tecnicasdealivioButton.setOnClickListener(view -> chamarTelaAlivio(view));
+        educacaoButton.setOnClickListener(view -> chamarTelaEducacao(view));
 
         BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
 
